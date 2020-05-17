4c2b1563765e72c62e0f42b7d98a49d3f371e8f1
vi <vi@windows10.microdone.cn>
2020-05-17 15:47:04 +0800

    change

--- demo/src/main/java/com/test/controller/UserController.java
+++ demo/src/main/java/com/test/controller/UserController.java
@@ -3,20 +3,21 @@ package com.test.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.dao.UserDao;
 import com.test.dto.User;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     private UserDao userDao;
 
     @RequestMapping("/query")
-	public List<User> queryUser(){
+	public String queryUser(){
     	  List<User> users = userDao.queryUserListById(1l);
-          return users;
+          return "login";
 	}
 }
