df911941d952b2a7d9a906188bd5237dac98c9c5
vi <vi@windows10.microdone.cn>
2020-05-17 17:14:32 +0800

    test

--- demo/src/main/java/com/test/dto/User.java
+++ demo/src/main/java/com/test/dto/User.java
@@ -1,44 +1,25 @@
 package com.test.dto;
 
 public class User {
-	private long id;
-	
-	private String name;
-	
-	private String password;
-	
-	private String sex;
-
-	public long getId() {
+	Integer id;
+	String username;
+	String password;
+	public Integer getId() {
 		return id;
 	}
-
-	public void setId(long id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
-
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	
 }
