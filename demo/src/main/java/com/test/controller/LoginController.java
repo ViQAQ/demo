218ca458f78f7a80e8a71e77aaf97b25a4a8cd12
vi <vi@windows10.microdone.cn>
2020-05-17 17:16:38 +0800

    a

--- demo/src/main/java/com/test/controller/LoginController.java
+++ demo/src/main/java/com/test/controller/LoginController.java
@@ -3,11 +3,13 @@ package com.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.test.dto.User;
+
 @Controller
 public class LoginController {
 	@RequestMapping("/login")
 	public String login() {
-		
+		User user = new User();
 		return "user";
 	}
 }
